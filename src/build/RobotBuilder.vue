4a7098d2370ba54632ec9d1a956f1f48b745ec78
Yohan Belléguic <yohan.belleguic@arkea.com>
2020-08-05 15:44:36 +0200

    i'm on vacation

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -55,7 +55,7 @@ import CollapsibleSection from '../shared/CollapsibleSection.vue';
 export default {
   name: 'RobotBuilder',
   created() {
-    this.$store.dispatch('getParts');
+    this.$store.dispatch('robots/getParts');
   },
   beforeRouteLeave(to, from, next) {
     if (this.addedToCart) {
@@ -82,7 +82,7 @@ export default {
   },
   computed: {
     availableParts() {
-      return this.$store.state.parts;
+      return this.$store.state.robots.parts;
     },
     saleBorderClass() {
       return this.selectedRobot.head.onSale ? 'sale-border' : '';
@@ -100,7 +100,7 @@ export default {
       const robot = this.selectedRobot;
       const cost = robot.head.cost + robot.leftArm.cost + robot.torsos.cost
        + robot.rightArm.cost + robot.bases.cost;
-      this.$store.commit('addRobotToCart', { ...robot, cost });
+      this.$store.dispatch('robots/addRobotToCart', { ...robot, cost }).then(() => this.$router.push('/cart'));
       this.addedToCart = true;
     },
   },
--- src/store/index.js
+++ src/store/index.js
@@ -1,33 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import axios from 'axios';
+
+import robotsModule from './modules/robots';
+import usersModule from './modules/users';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    cart: [],
-    parts: null,
-  },
-  mutations: {
-    addRobotToCart(state, robot) {
-      state.cart.push(robot);
-    },
-    updateParts(state, parts) {
-      state.parts = parts;
-    },
-  },
-  getters: {
-    cartSaleItems(state) {
-      return state.cart.filter((item) => item.head.onSale);
-    },
-  },
-  actions: {
-    getParts({ commit }) {
-      axios.get('/api/parts').then((result) => commit('updateParts', result.data))
-        .catch(console.error);
-    },
-  },
   modules: {
+    robots: robotsModule,
+    users: usersModule,
   },
 });
